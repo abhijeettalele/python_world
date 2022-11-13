4a86ac27d3f77ebce72e2410660b77bede1da170
abhijeettalele <38709270+abhijeettalele@users.noreply.github.com>
2022-11-13 12:02:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setuptools.setup(
     name="python_world",
     version="0.0.1",
     author="Cedd Burge",
-    author_email="ceddlyburge@gmail.com",
+    author_email="abhijeet.talele@gmail.com",
     description="A function that returns 'world'",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ceddlyburge/python_world",
+    url="https://github.com/abhijeettalele/python_world",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
